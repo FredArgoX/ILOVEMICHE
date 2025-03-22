758818f5de8fa66199ddff1fbd0164978955f7e2
FREDY ARCE GONZALEZ <fredy.argo.3@gmail.com>
2025-03-22 12:41:44 -0600

    Add files via upload

---
+++ js/social.js
@@ -0,0 +1,17 @@
+const authorBtn = document.querySelector(".author");
+const githubBtn = document.querySelector(".github");
+const linkedinBtn = document.querySelector(".linkedin");
+const xBtn = document.querySelector(".x");
+
+authorBtn.addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/fredy-arce-data-science/", "_blank");
+});
+githubBtn.addEventListener("click", () => {
+    window.open("https://github.com/FredArgoX", "_blank");
+});
+linkedinBtn.addEventListener("click", () => {
+    window.open("https://www.linkedin.com/in/fredy-arce-data-science/", "_blank");
+});
+xBtn.addEventListener("click", () => {
+    window.open("https://x.com/FredyArgoX", "_blank");
+});
